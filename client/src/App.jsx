37b2c7bf1ed9768b44824e666814f42a7476b51f
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-08-22 23:44:04 +0530

    chore: Add Vite and Tailwind CSS configuration files

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Homepage from './Homepage';
+function App() {
+ return (
+  <>
+      <h1 className = "text-4xl">App heading</h1>
+      <Homepage />
+  </>
+ )
+}
+
+export default App
---
+++ client/src/Homepage.jsx
@@ -0,0 +1,10 @@
+export default function Homepage() {
+  return (
+  <div className = "flex justify-center h-screen">
+      <div className = "bg-gray-300">
+
+      </div>
+      <h1 className = "text-4xl bg-gray-500">Homepage</h1>
+    </div>
+  )
+}

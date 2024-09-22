10a0541c3fbe9da70028fde85803ce567d9e31ce
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-09-22 22:11:54 +0530

    feat: Add utility functions and Card component; update Login form with Zod validation

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,19 +1,45 @@
-import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import {z} from 'zod';
 
-type FormFields = {
-    username: string;
-    email: string;
-    password: string;
+const schema = z.object({
+    username: z.string().min(4).max(25),
+    email: z.string().email(),
+    password: z.string().min(8),
+});
+
+type FormFields = z.infer<typeof schema>;
+
+// type FormFields = {
+//     username: string;
+//     email: string;
+//     password: string;
+// }
+
+const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
 }
 
 const Login = () => {
-    const form = useForm<FormFields>();
+    const {
+      register,
+      handleSubmit,
+      formState: { errors, isSubmitting },
+    } = useForm<FormFields>({
+        resolver: zodResolver(schema),
+    });
   return (
-    <form className="flex flex-col gap-4">
-        <input type="text" placeholder="Username" className="ring ring-blue-500 rounded-full" />
-        <input type="email" placeholder="Email" className="ring ring-blue-500 rounded-full" />
-        <input type="password" placeholder="Password" className="ring ring-blue-500 rounded-full" />
-        <button type="submit" className="bg-blue-500 text-white rounded-full">Login</button>
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("username")} type="text" placeholder="Username" className="ring ring-blue-500 rounded-full" />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+        <input {...register("email")} type="email" placeholder="Email" className="ring ring-blue-500 rounded-full" />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+        <input {...register("password")} type="password" placeholder="Password" className="ring ring-blue-500 rounded-full" />
+        {errors.password && <p className='text-red-500'>{errors.password.message}</p>} 
+        <button disabled={isSubmitting} type="submit" className="bg-blue-500 text-white rounded-full">
+            {isSubmitting ? "Loading": "Submit"}
+        </button>
     </form>
   )
 }

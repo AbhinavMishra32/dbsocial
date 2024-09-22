41b175ed374f8f17fe822fa3a423f05d4d5107dc
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-09-22 23:24:40 +0530

    feat: Enhance Login page with error handling and validation alerts; add Alert component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,11 +5,13 @@ import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} f
 import { Input } from "../components/ui/input";
 import { Label } from '../components/ui/label';
 import {Button} from "../components/ui/button";
+import { Alert, AlertDescription } from '../components/ui/alert';
+import { AlertCircle } from 'lucide-react';
 
 const schema = z.object({
-    username: z.string().min(4).max(25), // this is for sign up 
+    // username: z.string().min(4).max(25), // this is for sign up 
     email: z.string().email(),
-    password: z.string().min(8),
+    password: z.string().min(8, { message: "Password must contain at least 8 characters" }),
 });
 
 type FormFields = z.infer<typeof schema>;
@@ -31,43 +33,54 @@ const Login = () => {
       handleSubmit,
       formState: { errors, isSubmitting },
     } = useForm<FormFields>({
-        resolver: zodResolver(schema),
+        resolver: zodResolver(schema), // This makes it so that zod is the one validating if values are within rules or not. not using the default way hook forms do.
     });
   return (
-      <Card className="w-1/3 mx-auto mt-20">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>Login to access the website</CardDescription>
-        </CardHeader>
-            <form>
+    <Card className="w-1/3 mx-auto mt-20">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Login to access the website</CardDescription>
+      </CardHeader>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <CardContent>
-                <div className='grid w-full items-center gap-4'>
-                    <div className='flex flex-col space-y-1.5'>
-                        <Label htmlFor='name'>Email</Label>
-                        <Input {...register("email")} id="email" placeholder='Email' />
-                    </div>
-                    <div className='flex flex-col space-y-1.5'>
-                        <Label htmlFor='password'>Password</Label>
-                        <Input id="password" placeholder="Password" />
-                    </div>
-                </div>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Email</Label>
+              <Input {...register("email")} id="email" placeholder="Email" />
+              {errors.email && (
+                <Alert variant="destructive">
+                  <AlertCircle className="h-4 w-4"></AlertCircle>
+                  <AlertDescription>
+                    {errors.email.message && <p>{errors.email.message}</p>}
+                  </AlertDescription>
+                </Alert>
+              )}
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                {...register("password")}
+                id="password"
+                placeholder="Password"
+              />
+              {errors.password && (
+                <Alert variant="destructive">
+                  <AlertCircle className="h-4 w-4"></AlertCircle>
+                  <AlertDescription>
+                    {errors.password.message && (
+                      <p>{errors.password.message}</p>
+                    )}
+                  </AlertDescription>
+                </Alert>
+              )}
+            </div>
+          </div>
         </CardContent>
         <CardFooter>
-            <Button className='w-full'>Login</Button>
+          <Button disabled={isSubmitting} className="w-full">{isSubmitting? "Loading..." : "Login"}</Button>
         </CardFooter>
-            </form>
-      </Card>
-    // <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-    //     <input {...register("username")} type="text" placeholder="Username" className="ring ring-blue-500 rounded-full" />
-    //     {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-    //     <input {...register("email")} type="email" placeholder="Email" className="ring ring-blue-500 rounded-full" />
-    //     {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-    //     <input {...register("password")} type="password" placeholder="Password" className="ring ring-blue-500 rounded-full" />
-    //     {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-    //     <button disabled={isSubmitting} type="submit" className="bg-blue-500 text-white rounded-full">
-    //         {isSubmitting ? "Loading": "Submit"}
-    //     </button>
-    // </form>
+      </form>
+    </Card>
   );
 }
 

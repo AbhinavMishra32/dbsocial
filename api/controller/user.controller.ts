3d4fed26feaf5836cc7e7ed32d9453fc7c4aab19
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-09-22 13:04:06 +0530

    feat: Add user registration and login functionality with JWT authentication

--- api/controller/user.controller.ts
+++ api/controller/user.controller.ts
@@ -1,24 +1,37 @@
 import { Request, Response } from 'express';
 import {PrismaClient} from '@prisma/client';
 import { errorHandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { access } from 'fs';
 
 const prisma = new PrismaClient();
+const JWT_SECRET = process.env.JWT_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.JWT_SECRET;
+if (!JWT_SECRET || !REFRESH_TOKEN_SECRET) {
+    throw new Error("JWT_SECRET or REFRESH_TOKEN_SECRET is not defined");
+}
 
 export const getUsers = (req: Request, res: Response) => {
     const users = prisma.user.findMany();
     res.json(users);
 }
 
-export const createUser = async (req: Request, res: Response, next: Function) => {
+export const regUser = async (req: Request, res: Response, next: Function) => {
     try {
-    const { name, email } = req.body;
-    // console.log(name, email);
+    // console.log("Request body:", req);
+    const { name, email, password } = req.body;
+    const salt = bcryptjs.genSaltSync(10);
+    const hashedPassword = bcryptjs.hashSync(password, salt);
     const user = await prisma.user.create({
         data: {
-            name, email
+            name,
+            email,
+            password: hashedPassword
         }
     });
-    res.status(201).send({"Message": "User created successfully", user});
+
+    res.status(201).json({message: "User created successfully."});
     } catch (error) {
         if (error.code === 'P2002') {
             next(errorHandler(400, "User with this email already exists."));
@@ -27,3 +40,29 @@ export const createUser = async (req: Request, res: Response, next: Function) =>
         next(errorHandler(500, "An error occurred while creating the user. Please try again later."));
     }
 };
+
+export const loginUser = async(req: Request, res: Response, next: Function) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await prisma.user.findUnique({where: {name}});
+        if (!user) return res.status(401).json({message: "Invalid credentials"});
+
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+        if(!isPasswordValid) return res.status(401).json({message: "Invalid credentials"});
+
+        const accessToken = jwt.sign({userId: user.id}, JWT_SECRET, {expiresIn: '15m'});
+        const refreshToken = jwt.sign({userId: user.id}, REFRESH_TOKEN_SECRET, {expiresIn: '7d'});
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 7 * 24* 3600000,
+        })
+        res.json({accessToken});
+    } catch (error) {
+        next(errorHandler(500, "An error occurred while logging in. Please try again later."));
+    }
+}
+
+export const refreshToken = (req: Request, res: Response, next: Function) {
+})
--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getUsers, createUser } from "../controller/user.controller.js";
+import { getUsers, regUser, loginUser, refreshToken } from "../controller/user.controller.js";
 
 const router = Router();
 
 router.get("/", getUsers)
-.post("/", createUser);
+.post("/", regUser);
+
+router.post("/login", loginUser);
+
+router.post('/refresh-token', refreshToken);
 
 export default router;

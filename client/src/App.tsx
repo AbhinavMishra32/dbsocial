9468c11d735094089e9c9ae89fdb45a66da4f513
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-09-24 11:15:37 +0530

    Refactor layout and add sidebar component (working sidebar with routing)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,23 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import { UserProvider } from './context/UserContext';
 import Dashboard from './pages/Dashboard';
+import Layout from './components/Layout';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
     <UserProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
+        <Layout>
+          {/* <Sidebar> */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+          {/* </Sidebar> */}
+        </Layout>
       </BrowserRouter>
     </UserProvider>
   );

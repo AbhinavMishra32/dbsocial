e5833631e5a602564b51dd1b72b8102a62352dda
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-09-24 09:21:41 +0530

    added tsx in nodemon

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useUser } from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../components/ui/button';
-import {api} from '../services/axios';
+import { api } from '../services/axios';
 import PostsView from '../components/PostsView';
 
 const Dashboard = () => {
@@ -23,6 +23,7 @@ const Dashboard = () => {
 
   const handlePostSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await api.post(
         "/api/posts",
@@ -34,20 +35,22 @@ const Dashboard = () => {
         }
       );
       console.log("Response from server: ", response);
-      const updatedPosts = await api.get("/api/posts");
-      setPosts([...posts, ...updatedPosts.data]);
-      // setLoading(false);
+      setPosts((prevPosts) => [...prevPosts, response.data.post]);
+      setNewPost("");
+      setTitle("");
     } catch (err) {
       if (err.response?.status === 403) {
         setUser(null);
       }
       console.error("Error creating post: ", err.response);
-      setError(err.response?.data);
+      setError("Error creating post");
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    if(user?.token) {
+    if (user?.token) {
       const fetchPosts = async () => {
         try {
           const response = await api.get("/api/posts", {
@@ -63,11 +66,11 @@ const Dashboard = () => {
           }
           console.error("Error fetching posts: ", error);
         }
+        finally {
+          setLoading(false);
+        }
       };
       fetchPosts();
-    }
-    else {
-      setLoading(false);
     }
   }, [user, navigate]);
 
@@ -81,17 +84,17 @@ const Dashboard = () => {
         <p>Welcome {user?.username}</p>
         <p>Email: {user?.email}</p>
       </div>
-      <textarea onChange={(e) => {setNewPost(e.target.value)}} value={newPost} placeholder='Enter Post'>
+      <textarea onChange={(e) => { setNewPost(e.target.value) }} value={newPost} placeholder='Enter Post'>
       </textarea>
-      <input type = "text" onChange = {(e) => {setTitle(e.target.value)}} value = {title} placeholder='Title'/>
+      <input type="text" onChange={(e) => { setTitle(e.target.value) }} value={title} placeholder='Title' />
       <Button onClick={handlePostSubmit}>Post</Button>
       {error && (
         <div>
           <p>{error}</p>
         </div>
       )}
-        <PostsView posts={posts} isLoading={loading}/>
-  </>
+      <PostsView posts={posts} isLoading={loading} />
+    </>
   )
 }
 

bf909c4afd70ef9fb86e89faa0c6f8ebbaa0ba49
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-09-30 23:34:16 +0530

    Add comment to PostPage for future componentization of post

--- client/src/pages/PostPage.tsx
+++ client/src/pages/PostPage.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 const PostPage = () => {
     return (
         <div>
-
+            {/* Make a component of a post that can be used in different locations like in the post page */}
         </div>
     )
 }
